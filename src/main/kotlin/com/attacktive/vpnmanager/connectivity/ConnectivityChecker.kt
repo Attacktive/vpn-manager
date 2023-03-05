71d2b208f964e90b12971140c08cfe9984d73b17
Attacktive <welcome.hell.dude@gmail.com>
2023-03-05 22:05:26 +0900

    fix: `java.io.IOException: Connection reset`
    https://github.com/Attacktive/vpn-manager/issues/17

--- src/main/kotlin/com/attacktive/vpnmanager/connectivity/ConnectivityChecker.kt
+++ src/main/kotlin/com/attacktive/vpnmanager/connectivity/ConnectivityChecker.kt
@@ -1,10 +1,10 @@
 package com.attacktive.vpnmanager.connectivity
 
+import java.io.IOException
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodySubscribers
-import java.net.http.HttpTimeoutException
 import com.attacktive.vpnmanager.configuration.ConfigurationsService
 import org.slf4j.LoggerFactory
 
@@ -26,7 +26,7 @@ object ConnectivityChecker {
 
 			logger.debug("statusCode: $statusCode")
 			statusCode >= 400
-		} catch (e: HttpTimeoutException) {
+		} catch (e: IOException) {
 			logger.info("${e.message} reaching ${configurations.testUrl}")
 			true
 		}
