21c1768b5c4dd11b670a9eb39b9d31774cbf0e36
Attacktive <tribute2pantera@gmail.com>
2023-03-10 00:52:52 +0900

    chore: update module version to 1.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.attacktive"
-version = "1.1.3"
+version = "1.1.4"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/zsh
 
 ./build.sh
-java -jar build/libs/vpn-manager-1.1.3-uber.jar
+java -jar build/libs/vpn-manager-1.1.4-uber.jar
