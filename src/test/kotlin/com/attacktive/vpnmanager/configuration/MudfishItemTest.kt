5e66658d974a965ff833d107b81d6ae6f55aa2d4
Attacktive <tribute2pantera@gmail.com>
2023-03-11 15:45:34 +0900

    test: add test function about MudfishItem

--- src/test/kotlin/com/attacktive/vpnmanager/configuration/MudfishItemTest.kt
+++ src/test/kotlin/com/attacktive/vpnmanager/configuration/MudfishItemTest.kt
@@ -2,10 +2,13 @@ package com.attacktive.vpnmanager.configuration
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 class MudfishItemTest {
 	@Test
-	fun test() {
+	fun testGetToggleRequestBody() {
 		val mudfishItem = MudfishItem("doesn't matter", "whatever", false, "iid", "rid")
 		val toggleRequestBody = mudfishItem.getToggleRequestBody(true)
 
@@ -19,5 +22,20 @@ class MudfishItemTest {
 			""".trimIndent(),
 			toggleRequestBody
 		)
+	}
+
+	@Test
+	fun testIfEnabledIsTrueByDefault() {
+		val mudfishItemJson = """
+			{
+				"name": "jetbrains.com",
+				"testUrl": "https://youtrack.jetbrains.com/",
+				"iid": "217789",
+				"rid": "1475354"
+		}
+		""".trimIndent()
+
+		val mudfishItem: MudfishItem = Json.decodeFromString(mudfishItemJson)
+		assertTrue(mudfishItem.enabled)
 	}
 }
