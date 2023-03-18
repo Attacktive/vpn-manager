5b3b49914eed1b9f5d068d704a4d4354b3cd0633
Attacktive <tribute2pantera@gmail.com>
2023-03-19 02:53:25 +0900

    refactor: extract function to avoid deep nesting
    https://github.com/Attacktive/vpn-manager/issues/38

--- src/main/kotlin/com/attacktive/vpnmanager/configuration/ConfigurationsWatchService.kt
+++ src/main/kotlin/com/attacktive/vpnmanager/configuration/ConfigurationsWatchService.kt
@@ -3,36 +3,40 @@ package com.attacktive.vpnmanager.configuration
 import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.StandardWatchEventKinds
-import java.nio.file.WatchKey
+import java.nio.file.WatchService
 import kotlin.io.path.pathString
 import org.slf4j.LoggerFactory
 
 object ConfigurationsWatchService {
 	private val logger = LoggerFactory.getLogger(ConfigurationsWatchService.javaClass)
 
-	fun watch(function: () -> Unit) {
+	fun watchAndExecute(function: () -> Unit) {
 		val customConfigurationsPath = ConfigurationsService.getCustomConfigurationsPath()
+
 		FileSystems.getDefault()
 			.newWatchService()
 			.use {
 				customConfigurationsPath.register(it, StandardWatchEventKinds.ENTRY_MODIFY)
+				poll(it, function)
+			}
+	}
 
-				while (true) {
-					val watchKey: WatchKey = it.take()
-					for (event in watchKey.pollEvents()) {
-						// we only register "ENTRY_MODIFY" so the context is always a Path.
-						val changedFile = event.context() as Path
-						logger.debug("$changedFile has just been changed.")
+	private fun poll(watchService: WatchService, function: () -> Unit) {
+		while (true) {
+			val watchKey = watchService.take()
+			for (event in watchKey.pollEvents()) {
+				// we only register "ENTRY_MODIFY" so the context is always a Path.
+				val changedFile = event.context() as Path
+				logger.debug("$changedFile has just been changed.")
 
-						if (changedFile.pathString.endsWith(".json", true)) {
-							function()
-						}
-					}
-
-					if (!watchKey.reset()) {
-						logger.warn("The WatchKey has not been reset.")
-					}
+				if (changedFile.pathString.endsWith(".json", true)) {
+					function()
 				}
 			}
+
+			if (!watchKey.reset()) {
+				logger.warn("The WatchKey has not been reset.")
+			}
+		}
 	}
 }
