43c1e387ee245a3bced370e12349cfa4cf48d764
Attacktive <tribute2pantera@gmail.com>
2023-04-09 04:42:05 +0900

    fix: make scripts run from anywhere

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/zsh
 
+pushd "${0:a:h}"
 ./gradlew clean uberJar
+popd
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/zsh
 
+pushd "${0:a:h}"
 ./build.sh
 java -jar build/libs/vpn-manager-1.3.0-uber.jar
+popd
