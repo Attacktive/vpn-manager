856a4ab57eecb1044cdfb5b65a9970ffe95cd895
Attacktive <tribute2pantera@gmail.com>
2023-04-09 04:15:16 +0900

    refactor: throw more specific exception than RuntimeException
    https://github.com/Attacktive/vpn-manager/issues/51

--- src/main/kotlin/com/attacktive/vpnmanager/connectivity/ErrorsResponseDto.kt
+++ src/main/kotlin/com/attacktive/vpnmanager/connectivity/ErrorsResponseDto.kt
@@ -4,11 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ErrorsResponseDto(val errors: List<Error>) {
-	private val messages: List<String>
+	val messages: List<String>
 		get() = errors.map { it.message }
-
-	val mergedMessages: String
-		get() = messages.joinToString("\n")
 
 	@Serializable
 	data class Error(val message: String)
