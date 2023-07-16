8dfb6fabebbafe65e4a95b14cd9b952673ae3495
Attacktive <tribute2pantera@gmail.com>
2023-07-16 18:24:18 +0900

    hotfix: fix wrong script path

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 Push-Location $PSScriptRoot\..
-./build.ps1
-java -jar build/libs/vpn-manager-1.3.5-uber.jar
+.\build.ps1
+java -jar ..\build\libs\vpn-manager-1.3.5-uber.jar
 Pop-Location
