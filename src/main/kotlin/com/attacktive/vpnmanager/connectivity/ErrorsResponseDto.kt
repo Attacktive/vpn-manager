44aab5d6720b91298e45efa9eace10e77ca87898
Attacktive <tribute2pantera@gmail.com>
2024-09-28 17:09:58 +0900

    refactor: lift `return` out of `if`

--- src/main/kotlin/com/attacktive/vpnmanager/connectivity/ErrorsResponseDto.kt
+++ src/main/kotlin/com/attacktive/vpnmanager/connectivity/ErrorsResponseDto.kt
@@ -6,10 +6,10 @@ import kotlinx.serialization.Serializable
 data class ErrorsResponseDto(val error: String? = null, val errors: List<Error> = listOf()) {
 	val message: String?
 		get() {
-			if (errors.isEmpty()) {
-				return error
+			return if (errors.isEmpty()) {
+				error
 			} else {
-				return errors.joinToString("\n") { it.message }
+				errors.joinToString("\n") { it.message }
 			}
 		}
 
